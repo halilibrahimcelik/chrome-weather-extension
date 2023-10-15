ef59f0b6c1cc32d75ace5d7da8a6139c2703f216
halilibrahimcelik <halil.celik139@gmail.com>
2023-10-15 17:18:44 +0300

    feat:bug fixed while setting badgeText

--- app/background.ts
+++ app/background.ts
@@ -23,15 +23,6 @@ export default defineBackground(() => {
   chrome.storage.local.get(
     ["theme", "cityList", "tempScale"],
     (res: LocalStorage) => {
-      if (res.cityList) {
-        const homecityTemp = Math.round(res.cityList[0].main.temp);
-        const homecityUnit = res.tempScale === "metric" ? "°C" : "°F";
-
-        chrome.action.setBadgeText({
-          text: `${homecityTemp.toString()} ${homecityUnit?.toString()}`,
-        });
-      }
-
       chrome.storage.local.set({
         theme: "theme" in res ? res.theme : "light",
       });
