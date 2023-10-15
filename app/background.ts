f17dc00091ca8270e8e6caeb81f61923b88f3e1f
halilibrahimcelik <halil.celik139@gmail.com>
2023-10-15 17:04:01 +0300

    feat:underline text feature added

--- app/background.ts
+++ app/background.ts
@@ -1,4 +1,4 @@
-import { OpenweatherData } from "./popup/utils/api";
+import { OpenweatherData, fetchRequest } from "./popup/utils/api";
 
 export interface LocalStorage {
   theme?: string;
@@ -15,8 +15,8 @@ export interface LocalStorage {
 export default defineBackground(() => {
   chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
-      id: "example",
-      title: "Example",
+      id: "cityName",
+      title: "Add this city to wheather Card",
       contexts: ["all"],
     });
   });
@@ -38,6 +38,51 @@ export default defineBackground(() => {
       chrome.storage.local.set({
         popup: "popup" in res ? res.popup : false,
       });
+      chrome.storage.local.set({
+        error: "error" in res ? res.error : null,
+      });
     }
   );
+  chrome.contextMenus.onClicked.addListener((event) => {
+    if (event.menuItemId === "cityName") {
+      const query = event.selectionText?.toString();
+      fetchRequest(query!, "metric")
+        .then((res) => {
+          console.log(res.status, "status");
+          if (!res.ok) {
+            console.log("not found");
+            chrome.notifications.create("notfoundcity", {
+              type: "basic",
+              iconUrl: "https://img.icons8.com/emoji/48/warning-emoji.png",
+              title: "Warning",
+              message: "City not found please try a valid city name",
+            });
+            return;
+          }
+          return res.json();
+        })
+        .then((data: OpenweatherData) => {
+          chrome.storage.local.get(["cityList", "error"], (res) => {
+            for (const city of res.cityList!) {
+              console.log(city, "city name");
+              if (
+                data?.name?.trim().toLocaleLowerCase("tr-TR") ===
+                city?.name.trim().toLocaleLowerCase("tr-TR")
+              ) {
+                chrome.notifications.create("alreadyexist", {
+                  type: "basic",
+                  iconUrl: "https://img.icons8.com/emoji/48/warning-emoji.png",
+                  title: "Warning",
+                  message: "City already added to list",
+                });
+                return;
+              }
+            }
+            chrome.storage.local.set({
+              cityList: [...res.cityList!, data],
+            });
+          });
+        });
+    }
+  });
 });
